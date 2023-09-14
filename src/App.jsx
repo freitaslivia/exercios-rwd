fbe1233b864fc4abf64a280c03215ac361b8e147
freitaslivia <xliviafreitasx@gmail.com>
2023-09-14 16:21:31 -0300

    arquivo js criado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Menu from './components/Menu'
+import Rodape from './components/Rodape'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
